b77ec25dd8e702fb63453e8d67e53e32d7c0c896
Ido Bouskila <idobouskila22@gmail.com>
2023-08-20 12:48:11 +0300

    feat: add text animation on hero section

---
+++ js/hero.js
@@ -0,0 +1,22 @@
+// Hero Text Character Reveal Animation
+const heroHeaders = new SplitType('.hero-text-container *')
+
+gsap.to('.hero-text-container .char', {
+    y: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .5
+})
+
+// Hero Text Scroll Animation
+gsap.set('.hero-text-container, .scroll-text', { yPercent: 0 });
+
+const heroAnimation = gsap.to('.hero-text-container', { yPercent: -35 });
+
+ScrollTrigger.create({
+    trigger: '.hero',
+    start: 'top',
+    end: 'center',
+    animation: heroAnimation,
+    scrub: 1,
+});
