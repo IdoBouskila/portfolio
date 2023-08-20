36d0155f2e8d9141c11df632a4551dc868b40a8b
Ido Bouskila <idobouskila22@gmail.com>
2023-08-20 12:50:28 +0300

    feat: add smooth scroll effect with `Lenis`

--- js/main.js
+++ js/main.js
@@ -10,6 +10,19 @@ window.addEventListener('pointermove', (e) => {
     );
 });
 
+// Smooth Scroll effect
+const lenis = new Lenis({
+    duration: 2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+  })
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 function _(selector) {
     return document.querySelector(selector);
 }
