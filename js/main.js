43afc16c0889d5a7b604c58f86596772e4378356
Ido Bouskila <idobouskila22@gmail.com>
2023-08-21 16:40:51 +0300

    style: remove white-spaces and add semicolons

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ window.addEventListener('pointermove', (e) => {
 const lenis = new Lenis({
     duration: 2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  })
+});
 
 function raf(time) {
-    lenis.raf(time)
+    lenis.raf(time);
     requestAnimationFrame(raf);
 }
 
