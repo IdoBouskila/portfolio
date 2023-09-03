b8363c09f056873ca5a3ef74a8f81bbf2a50e95e
Ido Bouskila <idobouskila22@gmail.com>
2023-09-04 01:14:45 +0300

    fix: show scroll effects only on desktop devices

--- js/main.js
+++ js/main.js
@@ -7,44 +7,42 @@ gsap.registerPlugin(ScrollTrigger);
 
 const isMobileScreen = window.innerWidth <= 840;
 
-if(isMobileScreen) {
-    return;
-}
-
-const contactButton = _('.contact-btn');
-const blob = _('.blob');
-
-window.addEventListener('pointermove', (e) => {
-    blob.animate(
-        {
-            left: `${ e.clientX }px`,
-            top: `${ e.clientY }px`
-        },
-        { duration: 3500, fill: 'forwards' }
-    );
-});
-
-// Smooth Scroll effect
-const lenis = new Lenis({
-    duration: 2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
-
-function raf(time) {
-    lenis.raf(time);
+if(! isMobileScreen) {
+    const contactButton = _('.contact-btn');
+    const blob = _('.blob');
+    
+    window.addEventListener('pointermove', (e) => {
+        blob.animate(
+            {
+                left: `${ e.clientX }px`,
+                top: `${ e.clientY }px`
+            },
+            { duration: 3500, fill: 'forwards' }
+        );
+    });
+    
+    // Smooth Scroll effect
+    const lenis = new Lenis({
+        duration: 2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+    
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    
     requestAnimationFrame(raf);
+    
+    // integrate Lenis to GSAP ScrollTrigger
+    lenis.on('scroll', ScrollTrigger.update);
+    
+    gsap.ticker.add((time)=>{
+      lenis.raf(time * 1000);
+    });
+    
+    gsap.ticker.lagSmoothing(0);
+    
+    // navigate to contact section
+    contactButton.addEventListener('click', () => lenis.scrollTo('.contact', { duration: 5 }));
 }
-
-requestAnimationFrame(raf);
-
-// integrate Lenis to GSAP ScrollTrigger
-lenis.on('scroll', ScrollTrigger.update);
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000);
-});
-
-gsap.ticker.lagSmoothing(0);
-
-// navigate to contact section
-contactButton.addEventListener('click', () => lenis.scrollTo('.contact', { duration: 5 }));
